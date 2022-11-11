ccaecbcfc4b54dfe8a066fc05538de7bffaa647b
Michał Bar <logout400@gmail.com>
2022-11-11 20:26:09 +0100

    feat(lab3): add random slide button

--- lab3/zad3/script.js
+++ lab3/zad3/script.js
@@ -5,17 +5,28 @@ const sliderWrapper = slider.querySelector('.js-slider-wrapper');
 const slideCount = sliderWrapper.querySelectorAll('.js-slide').length;
 const prevButton = slider.querySelector('.js-prev');
 const nextButton = slider.querySelector('.js-next');
+const randomButton = document.querySelector('.js-random');
 
-const setCurrentSlide = (idx) => {
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const setCurrentSlideIdx = (idx) => {
   sliderWrapper.style.setProperty(currentSlideVar, idx);
 };
 
-const nextSlide = (direction) => {
-  const currentSlideIdx = Number(
+const getCurrentSlideIdx = () => {
+  return Number(
     getComputedStyle(sliderWrapper).getPropertyValue(currentSlideVar).trim()
   );
+};
+
+const nextSlide = (direction) => {
+  const currentSlideIdx = getCurrentSlideIdx();
   const nextSlideIdx = (currentSlideIdx + direction) % slideCount;
-  setCurrentSlide(nextSlideIdx < 0 ? slideCount - 1 : nextSlideIdx);
+  setCurrentSlideIdx(nextSlideIdx < 0 ? slideCount - 1 : nextSlideIdx);
 };
 
 prevButton.addEventListener('click', () => {
@@ -24,4 +35,15 @@ prevButton.addEventListener('click', () => {
 
 nextButton.addEventListener('click', () => {
   nextSlide(1);
+});
+
+randomButton.addEventListener('click', () => {
+  const currentSlideIdx = getCurrentSlideIdx();
+  let randomIdx = getRandomInt(0, slideCount);
+
+  while (randomIdx === currentSlideIdx) {
+    randomIdx = getRandomInt(0, slideCount);
+  }
+
+  setCurrentSlideIdx(randomIdx);
 });
