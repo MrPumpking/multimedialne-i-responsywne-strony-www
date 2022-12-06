90a5240e8dfd7697d6abfea0ca46bd59fcaa559a
Michał Bar <logout400@gmail.com>
2022-12-07 00:19:27 +0100

    feat(lab6): implement task 5

--- lab6/zad5/script.js
+++ lab6/zad5/script.js
@@ -1,5 +1,58 @@
 const canvas = document.querySelector('.js-canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
+const colorInput = document.querySelector('.js-color');
+let currentColor = colorInput.value;
+
+colorInput.addEventListener('change', (event) => {
+  currentColor = event.currentTarget.value;
+});
+
+const drawImage = () => {
+  const image = new Image();
+  image.onload = () => {
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+  };
+  image.src = './img/forest.jpg';
+};
+
+const addColor = () => {
+  ctx.save();
+  ctx.fillStyle = currentColor;
+  ctx.globalCompositeOperation = 'overlay';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.restore();
+};
+
+document.querySelector('.js-add-color').addEventListener('click', addColor);
+
+const cutFragment = () => {
+  const src = canvas.toDataURL('image/jpeg', 1.0);
+  const image = new Image();
+  image.onload = () => {
+    ctx.drawImage(image, 0, 0, 400, 225, 0, 0, canvas.width, canvas.height);
+  };
+  image.src = src;
+};
+
+document.querySelector('.js-cut').addEventListener('click', cutFragment);
+
+const addMask = () => {
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, Math.PI * 2);
+  ctx.clip();
+  addColor();
+  ctx.restore();
+};
+
+document.querySelector('.js-add-mask').addEventListener('click', addMask);
+
+const reset = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawImage();
+};
+
+document.querySelector('.js-reset').addEventListener('click', reset);
+
+drawImage();
