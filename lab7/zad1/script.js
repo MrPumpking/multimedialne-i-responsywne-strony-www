2a7d23681ff1cd7fb7822e7de8d23557d6c0d0f1
Michał Bar <logout400@gmail.com>
2022-12-13 14:33:14 +0100

    feat(lab7): implement task 1

---
+++ lab7/zad1/script.js
@@ -0,0 +1,29 @@
+let dragSrcEl = null;
+
+document.querySelectorAll('.drag-element').forEach((element) => {
+  element.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+
+  element.addEventListener('dragstart', (event) => {
+    dragSrcEl = event.currentTarget;
+    dragSrcEl.style.opacity = 0.4;
+
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', dragSrcEl.innerHTML);
+  });
+
+  element.addEventListener('dragend', (event) => {
+    event.currentTarget.style.opacity = 1;
+  });
+
+  element.addEventListener('drop', (event) => {
+    event.stopPropagation();
+    const dropTarget = event.currentTarget;
+
+    if (dragSrcEl !== dropTarget) {
+      dragSrcEl.innerHTML = dropTarget.innerHTML;
+      dropTarget.innerHTML = event.dataTransfer.getData('text/html');
+    }
+  });
+});
